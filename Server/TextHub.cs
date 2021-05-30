3cb45038125c4d8cc20d9b8b529d55e6a11ab10f
alpha0202 <alpha0202@gmail.com>
2021-05-30 14:24:01 -0500

    Agregar archivos de proyecto.

---
+++ Server/TextHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSignalR.Server
+{
+    public class TextHub : Hub
+    {
+        public async Task Send(string text)
+        {
+            await Clients.All.SendAsync("ReceiveInformation", text);
+
+        }
+
+    }
+}
